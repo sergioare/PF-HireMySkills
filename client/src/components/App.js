a62f9ba6211aec6ab10eddf252495a9fcf536a5c
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-01-18 20:30:54 -0300

    Install styles, finished NavBar, 1/2 Menu, Routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,30 @@
-import './App.css';
-import { Fragment } from 'react'
-import {Route, Routes} from 'react-router-dom'
-import Landing from './Landing/Landing'
-import Home from './Home/Home'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Landing from "./Landing/Landing";
+import Home from "./Home/Home";
+import NavBar from "./Navbar/Navbar";
+import About from "./About/About";
+import GeneralCategory from "./Categories/General/GeneralCategory";
+import FAQs from "./FAQs/FAQs";
+import FormStaff from "./Form/FormStaff";
+import JoinForm from "./Join/JoinForm";
+import SignIn from "./SignIn/SignIn";
 
 function App() {
   return (
-    <Fragment>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing/>} />
-        <Route path='/home' element={<Home/>} />
+        <Route path="/" element={<NavBar />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="categories" element={<GeneralCategory />} />
+          <Route path="FAQs" element={<FAQs />} />
+
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Route>
       </Routes>
-    </Fragment>
+    </BrowserRouter>
   );
 }
 
