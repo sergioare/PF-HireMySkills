078de8bb551d105d0bd67e0dea7cf02e3c9aa296
yuri-gomez12 <nallegova@gmail.com>
2023-01-18 20:06:35 -0500

    primer commit

---
+++ client/src/components/Categories/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    name: "SALUD",
+    categoriesSpecific: ["MEDICINA GENERAL", "ODONTOLOGIA", "TERAPIA FISICA"],
+  },
+  {
+    id: 2,
+    name: "ADMINISTRACION",
+    categoriesSpecific: ["SECRETARIA", "CONTABILIDAD", "GERENCIA"],
+  },
+  {
+    id: 3,
+    name: "INGENIERIA",
+    categoriesSpecific: ["ELECTRICA", "AUTOMOTRIZ", "AMBIENTAL"],
+  },
+  {
+    id: 4,
+    name: "HOGAR",
+    categoriesSpecific: ["LIMPIEZA", "CUIDAR ADULTOS", "PLOMERIA"],
+  },
+  {
+    id: 5,
+    name: "TECNOLOGIA",
+    categoriesSpecific: ["TELEFONIA", "INTERNET", "TELEVISION"],
+  },
+  {
+    id: 6,
+    name: "PROGRAMACION",
+    categoriesSpecific: ["DISEÑO WEB", "APLICACIONES", "SITIOS WEB"],
+  },
+];
