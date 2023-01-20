5cf20cc16d0c9cdf37ed9c3f59a7b91152a71b42
AndresAndrada <andresandrada1994@gmail.com>
2023-01-20 15:58:41 -0300

    Modificada la carpeta profession

--- server/src/models/profession.js
+++ server/src/models/profession.js
@@ -1,23 +1,18 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../db/db.js");
+const { DataTypes, } = require('sequelize');
 
-const Professions = sequelize.define(
-  "profession",
-  {
-    id: {
-      type: DataTypes.UUID, // este tipo de dato es para que no se repita con el de la API (234T324R23T)
-      defaultValue: DataTypes.UUIDV4, // y se genera de independencia
-      allowNull: false,
-      primaryKey: true,
-    },
-    profession: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+const Profession  =  (sequelize )=>{
+sequelize.define('profession', {
+    id : {
+        type: DataTypes.UUID,    // este tipo de dato es para que no se repita con el de la API (234T324R23T)
+        defaultValue: DataTypes.UUIDV4,   // y se genera de independencia  
+        allowNull: false,
+        primaryKey : true
+      },
+    profession :{
+        type: DataTypes.STRING,
+        allowNull : true
+      }
+})
+};
 
-module.exports = Professions;
+module.exports = Profession;
