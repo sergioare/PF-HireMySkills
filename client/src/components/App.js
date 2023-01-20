36a362ec5de833a43cb2272b863eccf45b5ade06
Sergio Arévalo <sergioarevalo301@gmail.com>
2023-01-19 22:00:26 -0500

    feat(src/components/Landing): add Landing component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,17 @@
-import './App.css';
 import { Fragment } from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Landing from './Landing/Landing'
 import Home from './Home/Home'
+import Team from './Team/Team';
 
 function App() {
   return (
+    
     <Fragment>
       <Routes>
         <Route path='/' element={<Landing/>} />
         <Route path='/home' element={<Home/>} />
+        <Route path='/creators' element={<Team/>}/>
       </Routes>
     </Fragment>
   );
