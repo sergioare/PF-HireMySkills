6e80ec2f13362bc79ffc33488ad7e75aefcae429
yuri-gomez12 <nallegova@gmail.com>
2023-01-20 08:06:02 -0500

    cambios en category

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,18 +1,11 @@
 import React from "react";
 import s from "../Footer/Footer.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
     <div className={s.Footer}>
       <h1 className={s.fooLogo}>HireMySkills</h1>
-      <div className={s.fooIcon}>
-        <FontAwesomeIcon icon="fa-brands fa-facebook" />
-        <FontAwesomeIcon icon="fa-brands fa-twitter" />
-        <FontAwesomeIcon icon="fa-brands fa-instagram" />
-        <FontAwesomeIcon icon="fa-brands fa-linkedin" />
-        <FontAwesomeIcon icon="fa-brands fa-github" />
-      </div>
+      <div className={s.fooIcon}>face whatsapp instagram linkedin github</div>
       <div className={s.fooDerch}>Derechos Reservados © Grupo 11 Henry</div>
     </div>
   );
