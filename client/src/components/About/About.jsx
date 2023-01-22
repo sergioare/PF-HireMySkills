1aeebc2c555734d68bb068d68936a9d9eeb37c20
AndresAndrada <andresandrada1994@gmail.com>
2023-01-22 15:44:01 -0300

    Rutas terminadas para la DEMO 1

--- client/src/components/About/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div>Esto es el about</div>;
-};
-
-export default About;
---
+++ server/src/controllers/deleteUser.js
@@ -0,0 +1,13 @@
+const users = require('../models/users');
+
+const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await users.update({ delete: true }, { where: { id: id } });
+        res.send({ message: 'User delete' });
+    } catch (error) {
+        res.send({ message: error })
+    }
+}
+
+module.exports = deleteUser;
