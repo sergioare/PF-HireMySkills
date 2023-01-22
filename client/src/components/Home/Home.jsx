7dd5f2e54c61880c8b078174b56ab971b4baf12b
yuri-gomez12 <nallegova@gmail.com>
2023-01-22 08:43:02 -0500

    Ajustes de componentes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,49 +1,45 @@
-import "./Home.css";
+import styles from "./Home.module.css";
 import NavBar from "../Navbar/Navbar";
-// import GeneralCategory from '../Categories/General/GeneralCategory.js'
 import GeneralCategory from "../Categories/General/GeneralCategory";
 import Footer from "../Footer/Footer";
-
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <div className="BigContainer_Home">
-        <div className="TextPro_Home">
+      <div className={styles.BigContainer_Home}>
+        <div className={styles.TextPro_Home}>
           FIND THE PERFECT PROFESSIONAL SERVICES FOR YOU
         </div>
-        <div className="SearchBar_Home">
+        <div className={styles.SearchBar_Home}>
           <input
             type="text"
             placeholder={`TRY "CARPENTER, DESIGNER, ELECTRICIAN"`}
-            className="SearchInput_Home"
+            className={styles.SearchInput_Home}
           />{" "}
-          <button type="submit" className="SearchButton_Home">
+          <button type="submit" className={styles.SearchButton_Home}>
             Search
           </button>
         </div>
-        <div className="Popular_Home">POPULAR SECTION</div>
+        <div className={styles.Popular_Home}>POPULAR SECTION</div>
         <div>
           <img
             src="https://media.istockphoto.com/id/1181830509/es/foto/urbano-feliz-mujer-de-negocios-usando-la-tableta-y-trabajando.jpg?s=612x612&w=0&k=20&c=p7EW36clLJUH6ICRXqYjic2VKMEcOfGP01SdI8nawkE="
             alt="Profesional"
-            className="Photo_Home"
+            className={styles.Photo_Home}
           />
         </div>
-        <div className="ProfesionalName_Home">Professional Name</div>
-        <div className="Stars_Home">⭐⭐⭐⭐⭐</div>
-        {/* /////////////////////////////////////////// */}
+        <div className={styles.ProfesionalName_Home}>Professional Name</div>
+        <div className={styles.Stars_Home}>⭐⭐⭐⭐⭐</div>
       </div>
-      <div className="divGeneral">
+
+      <div className={styles.divGeneral_Home}>
         <GeneralCategory />
       </div>
 
-      {/* <GeneralCategory/> */}
-      <div className="divFooter">
+      <div className={styles.divFooter_Home}>
         <Footer />
       </div>
-
     </div>
   );
 };
