5102918282436bf7dda07c479c41b85ddcc4e958
leonardogar83 <leonardogar.ec@gmail.com>
2023-01-24 16:32:50 -0500

    resueltos errores

--- server/src/controllers/Professions.js
+++ server/src/controllers/Professions.js
@@ -1,9 +1,13 @@
-const Professions = require("../models/profession.js");
+const Professions = require("../models/Profession.js");
 
 const postProfessions = async (req, res) => {
   let { profession, categoryId } = req.body;
-  await Professions.create({ profession, categoryId })
-    .then(res.json({ profession, categoryId }))
+  await Professions
+    .create({
+      profession,
+      categoryId,
+    })
+    .then(() => res.json({ profession }))
     .catch((err) => res.status(400).json(err.message));
 };
 module.exports = postProfessions;
