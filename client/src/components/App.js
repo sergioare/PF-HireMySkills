ae3b15e3f2e52b87c9c4bb31c1692818ea8b6aa2
Gaova777 <gaova777@utp.edu.co>
2023-01-25 10:42:59 -0500

    Fix (Css->Team) add:styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import GeneralCategory from "./Categories/General/GeneralCategory";
 import SpecificCategory from "./Categories/Specific/SpecificCategory";
 import Professionals from "./Categories/Proffesionals/Professionals";
 import FAQs from "./FAQs/FAQs";
+import Footer from "./Footer/Footer";
 
 import JoinForm from "./Join/JoinForm";
 
@@ -39,10 +40,12 @@ function App() {
 
           <Route path="/form" element={<Form />} />
           
+          
           {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
 
         </Routes>
       </Fragment>
+      <Footer/>
     </BrowserRouter>
   );
 }
