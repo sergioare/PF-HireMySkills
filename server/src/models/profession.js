9422cc38c8340f66587a54f9d3620b3515676998
121218952415 <adrian.alexis.vega@gmail.com>
2023-01-25 15:08:07 -0600

    back

--- server/src/models/profession.js
+++ server/src/models/profession.js
@@ -1,18 +1,23 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db/db.js")
+const sequelize = require("../db/db.js");
 
-
-const Profession = sequelize.define("profession", {
-  id: {
-    type: DataTypes.UUID, // este tipo de dato es para que no se repita con el de la API (234T324R23T)
-    defaultValue: DataTypes.UUIDV4, // y se genera de independencia
-    allowNull: false,
-    primaryKey: true,
+const professions = sequelize.define(
+  "profession",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    profession: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  profession: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  }, 
-});
+  {
+    timestamps: false,
+  }
+);
 
-module.exports = Profession;
+module.exports = professions;
