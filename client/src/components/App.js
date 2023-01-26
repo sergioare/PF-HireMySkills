af54c37e13a7637a8ff94fe8ba7a1ca37dc8e0ab
yuri-gomez12 <nallegova@gmail.com>
2023-01-26 11:24:43 -0500

    add compomente mail

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,41 +8,30 @@ import GeneralCategory from "./Categories/General/GeneralCategory";
 import SpecificCategory from "./Categories/Specific/SpecificCategory";
 import Professionals from "./Categories/Proffesionals/Professionals";
 import FAQs from "./FAQs/FAQs";
-import Footer from "./Footer/Footer";
-
 import JoinForm from "./Join/JoinForm";
-
 import SignIn from "./SignIn/Acc/SignIn";
 import Team from "./Team/Team";
-import NavBar from "./Navbar/Navbar";
-
-
-import Form from './Form/FormStaff'
+import Form from "./Form/FormStaff";
 import Contact from "./Contact-Us/Contact";
-
-
 
 function App() {
   return (
     <BrowserRouter>
       <Fragment>
         <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/creators" element={<Team />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/categories" element={<GeneralCategory />} />
-            <Route path="/categories/:id" element={<SpecificCategory />} />
-            <Route path="/professional/:id" element={<Professionals />} />
-            <Route path="/FAQs" element={<FAQs />} />
-            <Route path="/account" element={<SignIn />} />
-            <Route path="/contact-us" element={<Contact />} />
-            <Route path="/form" element={<Form />} />
-            {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
-
+          <Route path="/" element={<Landing />} />
+          <Route path="/creators" element={<Team />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/categories" element={<GeneralCategory />} />
+          <Route path="/categories/:id" element={<SpecificCategory />} />
+          <Route path="/professional/:id" element={<Professionals />} />
+          <Route path="/FAQs" element={<FAQs />} />
+          <Route path="/account" element={<SignIn />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/form" element={<Form />} />
         </Routes>
       </Fragment>
-      <Footer/>
     </BrowserRouter>
   );
 }
