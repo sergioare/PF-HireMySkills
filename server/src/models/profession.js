7d376e8bbde26ac2f2143d3be4a12ad2ece72c6b
yuri-gomez12 <nallegova@gmail.com>
2023-01-26 08:51:23 -0500

    modificaciones en el server

--- server/src/models/profession.js
+++ server/src/models/profession.js
@@ -4,7 +4,7 @@ const sequelize = require("../db/db.js");
 const Profession = sequelize.define("profession", {
   id: {
     type: DataTypes.UUID, // este tipo de dato es para que no se repita con el de la API (234T324R23T)
-    defaultValue: DataTypes.UUIDV4, // y se genera de independencia
+    defaultValue: DataTypes.UUIDV4, // y se genera de independencia, por defecto
     allowNull: false,
     primaryKey: true,
   },
