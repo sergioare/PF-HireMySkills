349a06c1046c0dec4a27a86d5a9964a478486e47
Sergio Arévalo <sergioarevalo301@gmail.com>
2023-01-26 20:57:44 -0500

    redux actions

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,22 +1,77 @@
-//ACTIONS TYPES
-export const GET_CATEGORIES = 'GET_CATEGORIES'
-export const GET_SUB_CATEGORY = 'GET_SUB_CATEGORY'
-export const GET_PROFESSIONALS = 'GET_PROFESSIONALS'
-export const GET_SERVICES = 'GET_SERVICES'
-export const SEARCH = 'SEARCH'
-export const POST_SERVICES = 'POST_SERVICES'
-export const POST_CATEGORIES = 'POST_CATEGORY'
-export const POST_SUB_CATEGORY = 'POST_SUB_CATEGORY'
-export const ORDER_BY_RATING = 'ORDER_BY_RATING'
-export const ORDER_BY_NAME = 'ORDER_BY_NAME'
-export const ORDER_BY_REVIEWS = 'ORDER_BY_REVIEWS'
-export const FILTER_BY_PROVINCE = 'FILTER_BY_PROVINCE'
-export const DELETE_SERVICE = 'DELETE_SERVICE'
+import axios from 'axios'
+import{
+    GET_CATEGORIES,
+    GET_SUB_CATEGORY,
+    GET_PROFESSIONALS,
+    GET_SERVICES,
+    SEARCH,
+    POST_SERVICES,
+    POST_CATEGORIES,
+    POST_SUB_CATEGORY,
+    POST_PROFESSIONALS,
+    POST_REVIEWS,
+    ORDER_BY_RATING,
+    ORDER_BY_NAME,
+    ORDER_BY_REVIEWS,
+    FILTER_BY_PROVINCE,
+    DELETE_SERVICE,
+    DELETE_PROFILE,
+    urlCategory,
+    urlProfession,
+    urlProfessionals,
+    urlUsers,
+    urlProducts,
+    urlReviews
+} from '../../utils'
+import api from '../../api.json'
 
-//URLS API
+export function getCategories(){
+    return async function (dispatch){
+        axios.get
+    }
+}
 
-export const urlCategory = 'http://localhost:4000/category'
-export const urlProfession = 'http://localhost:4000/profession'
-export const urlProfessionals = 'http://localhost:4000/professionals'
-export const urlUsers = 'http://localhost:4000/users'
-export const urlProducts = 'http://localhost:4000/products'
+export function getProfessionals(){
+    return async function(dispatch){
+        axios.get(urlProfessionals)
+        .then(res=>
+            dispatch({type:GET_PROFESSIONALS, payload:res.data}))
+    }
+}
+
+export function postProfessional(data){
+    return async function(dispatch){
+        axios.post(urlProfessionals, data)
+        .then(res=>
+            dispatch({type:POST_PROFESSIONALS, payload:res.data}))
+    }
+}
+
+export function deleteProfessional(id){
+    return async function(dispatch){
+        axios.delete(`${urlProfessionals}/${id}`)
+        .then(res=>
+            dispatch({type:DELETE_PROFILE, payload: res.data}))
+    }
+}
+export function orderByRating(payload){
+    return {
+        type: ORDER_BY_RATING,
+        payload,
+    }
+}
+
+export function orderByName(payload){
+    return {
+        type: ORDER_BY_NAME,
+        payload,
+    }
+}
+
+export function postReviews(message){
+    return async function(dispatch){
+        axios.post(urlReviews, message)
+        .then(res=>
+            dispatch({type:POST_REVIEWS, payload:res.data}))
+    }
+}
