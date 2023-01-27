181a1ef71c5b47610932c7a59d4ba0e212681ac3
AndresAndrada <andresandrada1994@gmail.com>
2023-01-26 22:09:29 -0300

    carpeta profession en moduls

--- server/src/models/profession.js
+++ server/src/models/profession.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require('../db/db.js');
-const Profession = sequelize.define('profession', {
+
+const Profession  =  (sequelize )=>{
+sequelize.define('profession', {
     id : {
         type: DataTypes.UUID,    // este tipo de dato es para que no se repita con el de la API (234T324R23T)
         defaultValue: DataTypes.UUIDV4,   // y se genera de independencia  
@@ -12,5 +13,6 @@ const Profession = sequelize.define('profession', {
         allowNull : true
       }
 })
+};
 
 module.exports = Profession;
