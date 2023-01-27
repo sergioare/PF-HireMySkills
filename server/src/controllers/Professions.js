ab41b622efd4ec3e1c0e53cf042a29fd9f4057d8
leonardogar83 <leonardogar.ec@gmail.com>
2023-01-27 18:46:07 -0500

    Get para profesiones sin ningun argumento (lista completa)

--- server/src/controllers/Professions.js
+++ server/src/controllers/Professions.js
@@ -5,31 +5,36 @@ const professionals = require("../models/professionals.js");
 const postProfessions = async (req, res) => {
   let { profession, categoryId } = req.body;
   try {
-    await Professions.create({ profession, categoryId })
-    res.json({ profession })    
-  } catch(error) {
+    await Professions.create({ profession, categoryId });
+    res.json({ profession });
+  } catch (error) {
     res.status(400).json(err.message);
   }
 };
 
 const searchProfession = async (req, res) => {
   const { profession } = req.query;
-  try {
-    const findProfessionals = await professionals.findAll();
-    // console.log(findProfessionals, 'PROFESIONAL');
-    const  result = findProfessionals.filter(pf => pf.skills.some(sk => sk.toLowerCase() === profession.toLowerCase()));
-    if(result.length) return res.send(result);
-    else return res.send({ message: 'Profession not found' })
-  } catch (error) {
-    res.send({ message: error });
-  };
+  if (!profession) {
+    const Prof_list = await Profession.findAll();
+    res.send(Prof_list);
+  } else {
+    try {
+      const findProfessionals = await professionals.findAll();
+      // console.log(findProfessionals, 'PROFESIONAL');
+      const result = findProfessionals.filter((pf) =>
+        pf.skills.some((sk) => sk.toLowerCase() === profession.toLowerCase())
+      );
+      if (result.length) return res.send(result);
+      else return res.send({ message: "Profession not found" });
+    } catch (error) {
+      res.send({ message: error });
+    }
+  }
 };
-
 module.exports = {
   postProfessions,
-  searchProfession
+  searchProfession,
 };
-
 
 // let recipedb = await Diets.findAll({
 //   where: { name: dietType },
--- server/src/controllers/getCategory.js
+++ server/src/controllers/getCategory.js
@@ -1,12 +1,12 @@
-const categories = require("../models/categories")
+const categories = require("../models/categories");
 
 const getCategory = async (req, res) => {
-    try {
-        const categoryes = await categories.findAll();
-        res.send(categoryes);
-    } catch (error) {
-        res.send({ message: error });
-    };
+  try {
+    const categories = await categories.findAll();
+    res.send(categories);
+  } catch (error) {
+    res.send({ message: error });
+  }
 };
 
 module.exports = { getCategory };
