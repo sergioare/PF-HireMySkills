ce590c1fd2b723c53cba0533faee494bd9fd14ca
AndresAndrada <andresandrada1994@gmail.com>
2023-01-27 14:22:23 -0300

    Cambios en db

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -5,7 +5,7 @@ config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:admin2020@localhost/HiMySkills`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     dialect: "postgres",
     native: false,
