b368644cebe1d9786c2cbfaa4e0e47eb8ce90f07
Sergio Arévalo <sergioarevalo301@gmail.com>
2023-01-29 14:40:30 -0500

    commited

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 1afe384106ffc2583c1d102e0c5f18c7d77f6005
 import axios from 'axios'
 import{
     GET_CATEGORIES,
@@ -38,36 +34,7 @@ import{
     urlShoppingcart
 } from '../../utils'
 import api from '../../api.json'
-<<<<<<< HEAD
-=======
-//ACTIONS TYPES
-export const GET_CATEGORIES = "GET_CATEGORIES";
-export const GET_SUB_CATEGORY = "GET_SUB_CATEGORY";
-export const GET_PROFESSIONALS = "GET_PROFESSIONALS";
-export const GET_SERVICES = "GET_SERVICES";
-export const SEARCH = "SEARCH";
-export const POST_SERVICES = "POST_SERVICES";
-export const POST_CATEGORIES = "POST_CATEGORY";
-export const POST_SUB_CATEGORY = "POST_SUB_CATEGORY";
-export const ORDER_BY_RATING = "ORDER_BY_RATING";
-export const ORDER_BY_NAME = "ORDER_BY_NAME";
-export const ORDER_BY_REVIEWS = "ORDER_BY_REVIEWS";
-export const FILTER_BY_PROVINCE = "FILTER_BY_PROVINCE";
-export const DELETE_SERVICE = "DELETE_SERVICE";
->>>>>>> bfdffbb25d19d39797b578473ac881c7ed251608
-=======
->>>>>>> 1afe384106ffc2583c1d102e0c5f18c7d77f6005
 
-export function getCategories(){
-    return async function (dispatch){
-        axios.get
-    }
-}
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1afe384106ffc2583c1d102e0c5f18c7d77f6005
 export function getProfessionals(){
     return async function(dispatch){
         axios.get(urlProfessionals)
@@ -171,13 +138,3 @@ export function deleteUser(id){
             dispatch({type:DELETE_USER, payload: res.data}))
     }
 }
-<<<<<<< HEAD
-=======
-export const urlCategory = "http://localhost:4000/category";
-export const urlProfession = "http://localhost:4000/profession";
-export const urlProfessionals = "http://localhost:4000/professionals";
-export const urlUsers = "http://localhost:4000/users";
-export const urlProducts = "http://localhost:4000/products";
->>>>>>> bfdffbb25d19d39797b578473ac881c7ed251608
-=======
->>>>>>> 1afe384106ffc2583c1d102e0c5f18c7d77f6005
