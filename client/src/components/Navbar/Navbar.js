8648b8b1351b23c08416ae1f9503af3433a812ff
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-02-04 19:03:14 -0300

    Change about and faqs b-w color

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,57 +1,62 @@
 import React from "react";
 import styles from "./NavBar.module.css";
 import Menu from "../Menu/Menu";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
- const shopping= useSelector((state) => state.shoppingCart);
- const values = shopping.map(service=>service.quantity).reduce((acc, element)=> acc +element, 0)
- const total = shopping.map(service=>service.quantity * service.price).reduce((acc, element)=> acc +element, 0)
+  const shopping = useSelector((state) => state.shoppingCart);
+  const values = shopping
+    .map((service) => service.quantity)
+    .reduce((acc, element) => acc + element, 0);
+  const total = shopping
+    .map((service) => service.quantity * service.price)
+    .reduce((acc, element) => acc + element, 0);
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   return (
-
     <div className={styles.BigContainer_NavBar}>
       <div className={styles.nabvar}>
-        
-          <Link to="/" className={styles.logo}>
-           HireMySkills
-          </Link>
+        <Link to="/" className={styles.logo}>
+          HireMySkills
+        </Link>
 
-          <Link to="/categories" className={styles.navLink}>
-         Categories
-          </Link>
+        <Link to="/categories" className={styles.navLink}>
+          Categories
+        </Link>
 
-          <Link to="/professionals" className={styles.navLink}>
-           Professionals
-          </Link>
+        <Link to="/professionals" className={styles.navLink}>
+          Professionals
+        </Link>
 
-          <Link to="/about" className={styles.navLink}>
-            About us
-          </Link>
-                     
+        <Link to="/about" className={styles.navLink}>
+          About us
+        </Link>
 
-          <Link to="/shopping" className={styles.navLink}>
-          <div id="cart-info" className="nav-info align-items-center cart-info d-flex justify-content-between mx-lg-5">
-            <span className="cart-info__icon mr-lg-3"><i className="fas fa-shopping-cart"></i></span>
-            <p className="mb-0 text-capitalize"><span id="item-count">{values} </span> Services - $<span className="item-total">{total}</span></p>
+        <Link to="/shopping" className={styles.navLink}>
+          <div
+            id="cart-info"
+            className="nav-info align-items-center cart-info d-flex justify-content-between mx-lg-5"
+          >
+            <span className="cart-info__icon mr-lg-3">
+              <i className="fas fa-shopping-cart"></i>
+            </span>
+            <p className="mb-0 text-capitalize">
+              <span id="item-count">{values} </span> Services - $
+              <span className="item-total">{total}</span>
+            </p>
           </div>
-          </Link>
-
-       
+        </Link>
 
         <Link to="/account" className={styles.navLink}>
           <i class="fa-solid fa-user px-1 fs-3"></i>
           {isLoading ? "Loading..." : isAuthenticated ? user.name : "Account"}
         </Link>
 
-
-        <div className={styles.menu}>
+        {/* <div className={styles.menu}>
           <Menu />
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- client/src/components/SignIn/Profile/Profile.js
+++ client/src/components/SignIn/Profile/Profile.js
@@ -3,30 +3,34 @@ import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./Profile.module.css";
 import { Link } from "react-router-dom";
 import UserType from "../../UserType/UserType";
+import Footer from "../../Footer/Footer";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && (
-      <div className={styles.Container_imgProfile}>
-        <img
-          src={user.picture}
-          alt="profile img"
-          style={{
-            width: "250px",
-            height: "400px",
-            border: "solid orange 5px",
-            borderRadius: "30%",
-          }}
-        />
-        <div className={styles.divPerfil}>
-          <h3>{user.name}</h3>
-          <h5>Email: {user.email}</h5>
-          <div>
-            <UserType />
+      <div>
+        <div className={styles.Container_imgProfile}>
+          <img
+            src={user.picture}
+            alt="profile img"
+            style={{
+              width: "250px",
+              height: "400px",
+              border: "solid orange 5px",
+              borderRadius: "30%",
+            }}
+          />
+          <div className={styles.divPerfil}>
+            <h3>{user.name}</h3>
+            <h5>Email: {user.email}</h5>
+            <div>
+              <UserType />
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
     )
   );
