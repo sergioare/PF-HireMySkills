dd9fda6ff2ce13a51b1c9ff7b60d14d784ef92eb
leonardogar83 <leonardogar.ec@gmail.com>
2023-02-04 11:18:05 -0500

    profession get by categoryId route added

--- server/src/routes/profession.js
+++ server/src/routes/profession.js
@@ -2,10 +2,15 @@ const Router = require("express");
 // const getDBInfo = require("../controllers/controllerOne");
 // const { professionsGetName } = require("../controllers/professionGetName");
 const router = Router();
-const { postProfessions, searchProfession } = require("../controllers/Professions.js");
+const {
+  postProfessions,
+  searchProfession,
+  searchProfessionId,
+} = require("../controllers/Professions.js");
 
 // GET
-router.get('/', searchProfession);
+router.get("/", searchProfession);
+router.get("/:id", searchProfessionId);
 
 // POST
 router.post("/", postProfessions);
