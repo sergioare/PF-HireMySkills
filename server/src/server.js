51afac8a4ad9feabf1f6799b29ef5a89a0cac369
leonardogar83 <112180486+leonardogar83@users.noreply.github.com>
2023-02-06 12:08:21 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ server.use(routes);
 
 (async function seqSync() {
   try {
-    sequelize.sync({ force: false }).then(() => {
+    sequelize.sync({ force: true }).then(() => {
       console.log("Postgres sync has been established successfully.");
     });
   } catch (error) {
