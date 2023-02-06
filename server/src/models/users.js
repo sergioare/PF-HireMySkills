e7b239782cf6e67842f2e975d3ea11adcad0b754
leonardogar83 <112180486+leonardogar83@users.noreply.github.com>
2023-02-06 11:28:11 -0500

    Update users.js

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -30,12 +30,12 @@ const users =
       town: {
         //lugar donde esta ubicado
         type: DataTypes.STRING,
-        // allowNull: true,
+         allowNull: true,
       },
       contact: {
         // numero del profesional
         type: DataTypes.STRING,
-        // allowNull: true,
+         allowNull: true,
       },
       deleted: {
         //borrado logico
